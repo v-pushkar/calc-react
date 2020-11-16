1e9b1222b63f4d0a97a028c0ce835cac7e1ba7f3
Valerii Pushkar <valerii.pushkar.work@gmail.com>
2020-11-16 14:50:23 +0100

    new tests add

--- src/utils/__test__/counter.test.js
+++ src/utils/__test__/counter.test.js
@@ -10,7 +10,7 @@ const toEqual=  ["125", "+","17","*"]
 const toSqrt=  ["25", "/"]
 
 
-describe("Counter tests",()=>{
+describe("Counter tests operators",()=>{
     const expected = []
     it("counter clianer operator must return []",()=>{
         expect(counter("number", "number", "C",prevValues)).toStrictEqual([])  
@@ -27,5 +27,32 @@ describe("Counter tests",()=>{
     it("count to square 25. Must return 5",()=>{
         expect(counter("operator", "operator", "=",toSqrt)).toStrictEqual([5])  
     })
+    it("if '+' inputed" ,()=>{
+        const expected = ["125", "+","17", "+"]
+        expect(counter("operator", "number", "+",prevValues)).toStrictEqual(expected)  
+    })
+    it("if '-' inputed" ,()=>{
+        const expected = ["125", "+","17", "-"]
+        expect(counter("operator", "number", "-",prevValues)).toStrictEqual(expected)  
+    })
+    it("if operator after operator inputed, '-' after '+':" ,()=>{
+        const expected = ["125", "+","17", "-"]
+        expect(counter("operator", "operator", "-",[...prevValues,"+"])).toStrictEqual(expected)  
+    })
 })
 
+
+describe("counter test input numbers:",()=>{
+    it("input numbers '125' on start - expected: [125]",()=>{
+        const expected = ["125"]
+        expect(counter("number", null, "125", [])).toStrictEqual(expected)
+    })
+    it("input numbers '32' after '14' on start - expected: [125]",()=>{
+        const expected = ["1432"]
+        expect(counter("number", "number", "32", ["14"])).toStrictEqual(expected)
+    })
+    it("input numbers '32' after '14' and operatot '+' on start - expected: [125]",()=>{
+        const expected = ["14","+","32"]
+        expect(counter("number", "operator", "32", ["14","+"])).toStrictEqual(expected)
+    })
+})
